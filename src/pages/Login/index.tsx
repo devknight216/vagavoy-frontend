f7b560616b2915e94d538b7b577e45c4540bdab4
XiaoYun <buper0719@gmail.com>
2022-09-29 12:02:12 +0300

    Build Button Component

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,7 @@
 import { useTheme } from '@mui/material'
 import { memo } from 'react'
 
-import { Avatar, Icon } from '../../components'
+import { Avatar, Button, Icon } from '../../components'
 
 export const Login = memo(() => {
   const theme = useTheme()
@@ -9,11 +9,22 @@ export const Login = memo(() => {
   return (
     <div>
       <Icon
-        iconName="Home"
+        iconName="Share"
         iconSize={32}
-        iconColor={theme.palette.green.dark}
+        iconColor={theme.palette.orange.dark}
       />
       <Avatar avatarSize={4} src="https://mui.com/static/images/avatar/2.jpg" />
+      <Button
+        variant="outlined"
+        buttonRightIconName="Share"
+        buttonFontBold={true}
+        buttonLabel="Share Profile"
+        sx={{
+          width: '100%',
+          backgroundColor: 'rgba(0, 51, 0, 0.1) !important',
+          border: '1px solid rgba(0, 51, 0, 0.2) !important'
+        }}
+      />
     </div>
   )
 })
