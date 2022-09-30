21e53e815991779246b6ff7fdf2a729d66887ca9
devknight216 <devknight216@gmail.com>
2022-09-30 18:10:30 +0100

    complete login modal

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,19 +1,22 @@
 import { Box } from '@mui/material'
-import { memo } from 'react'
+import React, { memo } from 'react'
 import { Outlet } from 'react-router-dom'
 
+import LoginModal from '../Auth/Login'
 import { Header } from '../index'
 
 export const AppLayout = memo(() => {
+  const [login, setLogin] = React.useState(false)
   return (
     <div className="w-full flex flex-col">
       <div className="w-full mx-auto lg:max-w-[1200px]">
-        <Header />
+        <Header onLogin={() => setLogin(true)} />
       </div>
 
       <Box sx={{ display: 'flex', flexDirection: 'row', flex: 1 }}>
         <Outlet />
       </Box>
+      <LoginModal open={login} onClose={() => setLogin(false)} />
     </div>
   )
 })
