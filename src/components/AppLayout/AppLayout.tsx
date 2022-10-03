aec6ec3972c982481ae44c6bbfe85c976a70beaf
devknight216 <devknight216@gmail.com>
2022-10-03 08:50:23 +0100

    add signout modal

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -3,20 +3,23 @@ import React, { memo } from 'react'
 import { Outlet } from 'react-router-dom'
 
 import LoginModal from '../Auth/Login'
+import SignOutModal from '../Auth/SignOut'
 import { Header } from '../index'
 
 export const AppLayout = memo(() => {
   const [login, setLogin] = React.useState(false)
+  const [signout, setSignout] = React.useState(false)
   return (
     <div className="w-full flex flex-col bg-[#F5F5F5]">
       <div className="w-full mx-auto xl:max-w-[1200px]">
-        <Header onLogin={() => setLogin(true)} />
+        <Header onLogin={() => setLogin(true)} onSignOut={() => setSignout(true)} />
       </div>
 
       <Box sx={{ display: 'flex', flexDirection: 'row', flex: 1 }}>
         <Outlet />
       </Box>
       <LoginModal open={login} onClose={() => setLogin(false)} />
+      <SignOutModal open={signout} onClose={() => setSignout(false)} />
     </div>
   )
 })
---
+++ src/components/Auth/SignOut.tsx
@@ -0,0 +1,88 @@
+import { Close } from '@mui/icons-material'
+import {
+  Checkbox,
+  Dialog,
+  DialogProps,
+  FormControlLabel,
+  IconButton,
+  Paper,
+  styled,
+  useTheme
+} from '@mui/material'
+import React from 'react'
+
+import Button from '../Button'
+import TextField from '../TextField'
+
+interface SignOutModalProps extends DialogProps {
+  onClose: () => void
+}
+
+const BorderButton = styled(IconButton)`
+  border: 1px solid var(--var-green-light1);
+  color: black;
+`
+
+const StyledPaper = styled(Paper)`
+  flex-direction: row !important;
+  border-radius: 16px;
+  max-width: 980px !important;
+  width: 100%;
+  @media (max-width: 1199px) {
+    max-width: 575px !important;
+  }
+`
+
+const SignOutModal: React.FC<SignOutModalProps> = ({ open, onClose }) => {
+  const theme = useTheme()
+  return (
+    <Dialog open={open} onClose={() => onClose()} PaperComponent={StyledPaper}>
+      <img
+        src="/images/login.jpg"
+        className="w-[480px] h-auto hidden xl:block"
+      />
+      <div className="max-w-[575px] w-full px-4 pt-5 xl:pl-[60px] sm:pr-8 min-h-[500px]">
+        <div className="flex flex-row-reverse">
+          <BorderButton
+            className="w-8 h-8 sm:w-11 sm:h-11"
+            onClick={() => onClose()}>
+            <Close />
+          </BorderButton>
+        </div>
+        <div className="xl:pr-20 flex flex-col items-center mx-auto max-w-[320px] xl:max-w-none w-full">
+          <span className="text-[28px] font-semibold py-5">Welcome Back!</span>
+          <div className="items-start w-full mb-4">
+            <div className="text-[14px] text-green-500 mb-1.5">Full Name</div>
+            <TextField textFieldHeight={44} fullWidth />
+          </div>
+          <div className="items-start w-full mb-4">
+            <div className="text-[14px] text-green-500 mb-1.5">Email</div>
+            <TextField textFieldHeight={44} fullWidth />
+          </div>
+          <div className="items-start w-full mb-4">
+            <div className="text-[14px] text-green-500 pb-1.5">Pasword</div>
+            <TextField textFieldHeight={44} type="password" fullWidth />
+          </div>
+          <div className="items-start mb-4">
+            <FormControlLabel
+              control={
+                <Checkbox
+                  sx={{
+                    color: theme.palette.green.dark,
+                    '&.Mui-checked': {
+                      color: theme.palette.green.dark
+                    }
+                  }}
+                />
+              }
+              label="I accept the User Agreement"
+            />
+          </div>
+          <Button fullWidth variant="contained" buttonLabel="Sign In" />
+        </div>
+      </div>
+    </Dialog>
+  )
+}
+
+export default SignOutModal
