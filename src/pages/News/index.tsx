015569b9e3930971e64c5075af3f0ca503712c67
xiao <buper0719@gmail.com>
2022-11-08 20:33:20 +0000

    Bug fix in NewsFeed page

--- src/pages/News/index.tsx
+++ src/pages/News/index.tsx
@@ -43,7 +43,7 @@ export const News = memo(() => {
 
   return (
     <MainContainer className="w-full min-h-[calc(100vh-80px)]">
-      <div className="flex flex-row xl:gap-x-4 mt-8 xl:px-6 justify-center">
+      <div className="flex flex-row xl:gap-x-4 mt-8 xl:pl-6 justify-center">
         <div className="xl:w-2/3 flex flex-col items-start w-full max-w-[800px]">
           <span className="font-semibold text-[28px] leading-6 mb-8">
             Newsfeed
@@ -96,37 +96,39 @@ export const News = memo(() => {
             tripLogs.map((tripLog, index) => (
               <div
                 key={tripLog.tripLogId || index}
-                className="flex flex-row gap-x-4 mb-4">
+                className="w-full flex flex-row gap-x-[14px] mb-4">
                 {tripLog.tripGallery && tripLog.tripGallery?.length > 0 ? (
                   <img
                     src={tripLog.tripGallery[0].src}
                     alt={tripLog.tripGallery[0].backgroundInfo}
                     loading="lazy"
-                    className="w-[144px] h-[104px]"
+                    className="max-w-[144px] h-[104px] w-[144px] rounded-md"
                   />
                 ) : (
                   <></>
                 )}
-                <div className="w-full flex flex-row gap-x-[14px]">
-                  <Avatar
-                    src={profile.profileImage}
-                    className="w-[44px] h-[44px]"
-                  />
-                  <div className="flex flex-col">
-                    <span className="font-bold text-lg leading-6 text-green-700 text-left">
-                      {profile.mainInfo?.name}
-                    </span>
-                    <span className="font text[14px] leading-[21px] text-green-500 text-left mb-1">
-                      {profile.mainInfo?.location}
-                    </span>
-                    <div className='text-left'>
-                      <TextTruncate
-                        line={2}
-                        element="span"
-                        truncateText="..."
-                        text={tripLog.tripDescription}
-                      />
+                <div className='flex flex-1 flex-col gap-y-2'>
+                  <div className="flex flex-row gap-x-[14px]">
+                    <Avatar
+                      src={profile.profileImage}
+                      className="w-[44px] h-[44px]"
+                    />
+                    <div className="w-full flex flex-col text-left">
+                      <span className="font-bold text-lg leading-6 text-green-700 text-left">
+                        {profile.mainInfo?.name}
+                      </span>
+                      <span className="font text[14px] leading-[21px] text-green-500 text-left">
+                        {profile.mainInfo?.location}
+                      </span>
                     </div>
+                  </div>
+                  <div className="text-green-700 text-[16px] leading-6 text-left">
+                    <TextTruncate
+                      line={2}
+                      element="span"
+                      truncateText="..."
+                      text={tripLog.tripDescription}
+                    />
                   </div>
                 </div>
               </div>
