e481ff7ea3a2f76fa3de00caa2071703e104974e
xiao <buper0719@gmail.com>
2022-11-09 07:55:46 +0000

    Avatar Update Issue

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,6 +1,11 @@
 import { Box } from '@mui/material'
-import React, { memo } from 'react'
+import { AxiosError } from 'axios'
+import React, { memo, useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
+import { useAuth, useToast } from 'src/hooks'
+import { axiosInstance } from 'src/services/jwtService'
+import { updateProfile } from 'src/store/reducers/accountSlice'
+import { useAppDispatch } from 'src/store/store'
 
 import LoginModal from '../Auth/Login'
 import SignUpModal from '../Auth/SignUp'
@@ -9,6 +14,27 @@ import { Header } from '../index'
 export const AppLayout = memo(() => {
   const [login, setLogin] = React.useState(false)
   const [signup, setSignup] = React.useState(false)
+  const { user } = useAuth()
+  const { showToast } = useToast()
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    const userId = user.id
+    console.log(userId)
+    if (userId) {
+      axiosInstance
+        .get(`${process.env.REACT_APP_API_URL}/user/${userId}`)
+        .then((res) => {
+          dispatch(updateProfile(res.data))
+        })
+        .catch((err: AxiosError) => {
+          showToast({
+            type: 'error',
+            message: err.response?.data
+          })
+        })
+    }
+  }, [user])
   return (
     <div className="w-full flex flex-col bg-[#F5F5F5]">
       <div className="w-full mx-auto xl:max-w-[1200px]">
